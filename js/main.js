97398d90a136c3dd41d2f49a16107c40b85d85b6
Ilya <86313716+eqodel@users.noreply.github.com>
2022-03-31 18:15:06 +0300

    add responsive

--- js/main.js
+++ js/main.js
@@ -31,3 +31,38 @@ modalClose.addEventListener('click', () => {
     html.classList.remove('nopadding');
     header.classList.remove('nopadding-header');
 });
+
+let date = new Date('Apr 29 2022 00:00:00');
+
+function counts() {
+    let now = new Date();
+    gap =  date - now
+
+    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(gap / 1000 / 60 / 60 ) % 24;
+    let minutes = Math.floor(gap / 1000 / 60 ) % 60;
+    let seconds = Math.floor(gap / 1000 ) % 60;
+
+    if(gap < 0) {
+        days = days + 7;
+        hours = hours + 24;
+        minutes = minutes + 60;
+        seconds = seconds + 60;
+    }
+
+    document.getElementById('d').innerText = days;
+    document.getElementById('h').innerText = hours;
+    document.getElementById('m').innerText = minutes;
+    document.getElementById('s').innerText = seconds;
+}
+counts();
+
+setInterval(counts, 1000);
+
+var menuButton = $('.menu__button');
+  menuButton.on('click', function () {
+    $('.header__nav').toggleClass('header__nav--visible');
+    $('.header').toggleClass('header--menu');
+    $('.header__wrap').toggleClass('header__wrap--menu');
+    $('.header__nav').toggleClass('header__nav--menu');
+  });
